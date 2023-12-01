b44d54f46170c465cb01d883d4f9c370e780b0f8
bintangryan <143849730+bintangryan@users.noreply.github.com>
2023-12-01 17:50:06 +0700

    Update about.vue

--- nuxt/pages/about.vue
+++ nuxt/pages/about.vue
@@ -1,3 +1,123 @@
 <template>
-  <p>halo</p>
+  <div>
+    <Navbar></Navbar>
+    <div class="about">
+      <main class="max-w-screen-xl mx-auto grid gap-0">
+        <!-- Informasi tentang satu orang -->
+        <section id="bg-page" class="text-center min-h-screen bg-cover bg-center" style="background-image: url('https://images.pexels.com/photos/2422294/pexels-photo-2422294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');">
+          <div class="flex justify-center items-center h-full">
+            <div class="p-0 shadow-md dark:shadow-xl rounded-md">
+              <!-- Konten di dalam section (jika ada) -->
+              <h2 class="big-title">Meet Our Team</h2>
+            </div>
+          </div>
+        </section>
+        <section id="profile" class="text-center grid place-items-center mt-10">
+        <div class="grid gap-4" style="display: flex; align-items: center;">
+          <div style="margin-right: 200px;">
+            <img
+              src="../assets/imgs/ara.jpg"
+              alt="John Doe Image"
+              class="h-60 w-60 rounded-full"
+            />
+          </div>
+          <div>
+            <h2 class="text-4xl font-bold" style="color: #fbd914">
+              Mutiara Nurhaliza
+            </h2>
+            <h3 style="color: #fff2ab; font-size: 28px;">
+              5027221010
+            </h3>
+            <p class="w-full max-w-lg" style="font-size: 20px;">
+              "I will do my best to create unique, aesthetic, and comfy furnitures for you"
+            </p>
+          </div>
+        </div>
+      </section>
+        <section id="profile" class="text-center grid place-items-center mt-10">
+        <div class="grid gap-4" style="display: flex; align-items: center;">
+          <div>
+            <h2 class="text-4xl font-bold" style="color: #fbd914">
+              Bintang Ryan Wardana
+            </h2>
+            <h3 style="color: #fff2ab; font-size: 28px;">
+              5027221022
+            </h3>
+            <p class="w-full max-w-lg" style="font-size: 20px;">
+              "Affordable furnitures ? I could handle that. Trust me, I'm the one who design an aesthetic and affordable furnitures for you"
+            </p>
+          </div>
+          <div style="margin-left: 200px;">
+            <img
+              src="../assets/imgs/bintang.jpg"
+              alt="John Doe Image"
+              class="h-60 w-60 rounded-full"
+            />
+          </div>
+        </div>
+      </section>
+
+        <section id="profile" class="text-center grid place-items-center mt-10">
+        <div class="grid gap-4" style="display: flex; align-items: center;">
+          <div style="margin-right: 200px;">
+            <img
+              src="../assets/imgs/krisna.jpg"
+              alt="John Doe Image"
+              class="h-60 w-60 rounded-full"
+            />
+          </div>
+          <div>
+            <h2 class="text-4xl font-bold" style="color: #fbd914;" >
+              Marselinus Krisnawan Riandika            
+            </h2>
+            <h3 style="color: #fff2ab; font-size: 28px;">
+              5027221056
+            </h3>
+            <p class="w-full max-w-lg" style="font-size: 20px;">
+              "If you like something abstract and absurd, it's absolutely my design. Abstract design for furniture always the best"
+            </p>
+          </div>
+        </div>
+      </section>
+
+
+        <!-- Tambahkan bagian-bagian lain sesuai kebutuhan -->
+
+      </main>
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
+
+<script>
+import Navbar from '~/components/Navbar.vue';
+import Footer from '~/components/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+};
+</script>
+
+<style scoped>
+.about {
+  text-align: center;
+}
+
+.big-title{
+  font-size: 100px;
+  font-weight: 600;
+}
+
+section {
+  height: 600px;
+  padding: 0;
+  margin: 0;
+}
+
+#profile{
+  height: 400px;
+}
+</style>
